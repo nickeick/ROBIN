a8f01dcb0fbf297638c8ba1794387a5d7abfad42
nickeick <eickh032@umn.edu>
2023-03-28 02:31:12 -0500

    Testing connections

--- discord_bot.py
+++ discord_bot.py
@@ -2080,20 +2080,28 @@ When is it? How often is it? Where can I learn more? Answer: Check #announcement
         try:
             user = message.author
             voice_channel = user.voice.channel
+            await self.debug("conn1")
             assert voice_channel != None, "You are not in a voice channel"
             if str(user.voice.channel.id) in self.vc.keys():
                 pass
             else:
+                await self.debug("conn2")
                 for voice_client in self.voice_clients:
                     if voice_client.guild == message.guild:
+                        await self.debug("conn3")
                         assert not (voice_client.is_playing() or voice_client.is_paused()), "I am currently active in another voice channel. Thank you for your patience"
                         await voice_client.disconnect()
+                        await self.debug("conn4")
                         if str(message.author.voice.channel.id) in self.vc:
                             del self.vc[str(message.author.voice.channel.id)]
+                            await self.debug("conn5")
                         if message.id in self.waiting_channels:
                             self.waiting_channels.remove(message.id)
+                await self.debug("conn6")
                 voice_obj = await voice_channel.connect()
+                await self.debug("conn7")
                 self.vc[str(user.voice.channel.id)] = voice_obj
+                await self.debug("conn8")
                 #await message.channel.send('Robin has connected')
         except AssertionError as err:
             if message.id not in self.waiting_channels:
