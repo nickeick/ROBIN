2cd26fca43261a14a4312ad3f3a34e199ad39686
nickeick <eickh032@umn.edu>
2023-04-20 23:36:24 -0500

    Commenting the queueing tests

--- discord_bot.py
+++ discord_bot.py
@@ -2290,24 +2290,24 @@ When is it? How often is it? Where can I learn more? Answer: Check #announcement
                 self.next_song = self.song_queue.pop()  #self.next_song: (url, channel_id, message)
                 #print("jukebox 2")
         if self.next_song != None:
-            await self.debug("jukebox 3")
+            #await self.debug("jukebox 3")
             #if self.next_song[1] in self.vc.keys():
             # You removed the indent here
             voice_client = self.next_song[2].guild.voice_client
             #await self.debug(str(voice_client.is_connected()))
             #voice_client = self.vc[self.next_song[1]]
             if voice_client.is_connected() and not (voice_client.is_playing() or voice_client.is_paused() and not self.looping):
-                await self.debug("jukebox 5")
+                #await self.debug("jukebox 5")
                 await self.vc_play_song(self.next_song[0], self.next_song[2])
                 self.next_song = None
-            else:
+            if not voice_client.is_connected():
                 if len(self.next_song[2].author.voice.channel.members) < 1:        #Tests if Robin is alone
-                    await self.debug("jukebox 6")
+                    #await self.debug("jukebox 6")
                     await self.vc_disconnect(self.next_song[2])
                     self.song_queue = []
                 else:
                     await self.vc_connect(self.next_song[2])
-                    await self.debug("jukebox 7")
+                    #await self.debug("jukebox 7")
 
 
     @loop(seconds = 5)
