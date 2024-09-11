ae124ed9b5111430a210b671d3caf50a8a334605
Jacob <jskoshiol77@yahoo.com>
2024-09-11 18:18:07 -0500

    update play command

--- src/cogs/play_cog.py
+++ src/cogs/play_cog.py
@@ -11,14 +11,14 @@ class PlayView(discord.ui.View): # Create a class called PlayView that subclasse
     async def yes_button_callback(self, interaction: Interaction, button):
         await interaction.user.add_roles(interaction.guild.get_role(757388821540372561)) # Add user to the "Yes" role
         if interaction.user.name not in interaction.message.content: # If user is listed as a "yes" and presses "No," remove from "yes"
-            await interaction.message.edit(content=interaction.message.content + '\n*' + interaction.user.name + '*')
+            await interaction.message.edit(content=interaction.message.content + '\n*' + interaction.user.mention + '*')
         await interaction.response.defer()
 
     @discord.ui.button(label="No", style=discord.ButtonStyle.red) # Create a button with the label "No" with color red
     async def no_button_callback(self, interaction: Interaction, button):
         await interaction.user.add_roles(interaction.guild.get_role(757389176449531954)) # Add user to the "No" role
         if interaction.user.name in interaction.message.content: # If user is listed as a "yes" and presses "No," remove from "yes"
-            await interaction.message.edit(content=interaction.message.content.replace('\n*' + interaction.user.name + '*', ''))
+            await interaction.message.edit(content=interaction.message.content.replace('\n*' + interaction.user.mention + '*', ''))
         await interaction.response.defer()
 
 class PlayCog(commands.Cog):
@@ -37,7 +37,7 @@ class PlayCog(commands.Cog):
             for member in guild.get_role(id_type).members:
                 await member.remove_roles(guild.get_role(id_type))
 
-    @app_commands.command()
+    @app_commands.command(description= 'Ask people if they are interested in playing a game with you')
     async def play(self, interaction: Interaction, time: str):
         await self.remove_roles(interaction.guild)
         mention = ""
@@ -45,9 +45,9 @@ class PlayCog(commands.Cog):
                 if role.name.lower() == interaction.channel.name.replace('-', ' '):
                     mention = role.mention
         if mention == "":
-            sent = await interaction.response.send_message('Dojo, is anyone interested in playing ' + time + "\n\nYesses:\n*" + interaction.user.name + '*', view=PlayView())
+            sent = await interaction.response.send_message('Dojo, is anyone interested in playing ' + time + "\n\nYesses:\n*" + interaction.user.mention + '*', view=PlayView())
         else:
-            sent = await interaction.response.send_message(mention + 'is anyone interested in playing ' + time + "\n\nYesses:*" + interaction.user.name + '*', view=PlayView())
+            sent = await interaction.response.send_message(mention + 'is anyone interested in playing ' + time + "\n\nYesses:*" + interaction.user.mention + '*', view=PlayView())
 
         
 
