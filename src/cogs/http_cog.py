20bfe5bce52fa5c3bb74083dbd67312d05bfb9bd
Nick Eickhoff <nickeick@comcast.net>
2025-01-12 23:31:13 -0600

    fix the web app startup to a method

--- src/cogs/http_cog.py
+++ src/cogs/http_cog.py
@@ -9,22 +9,29 @@ IS_ENABLED = True
 class HTTPCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        app = web.Application()
-        app.add_routes([web.get('/button', self.handle)])
+        self.app = web.Application()
+        self.app.add_routes([web.get('/button', self.handle)])
 
     async def cog_check(self, ctx: Context):
         if (not IS_ENABLED):
             await ctx.send('This Cog is disabled')
         return IS_ENABLED
 
-    async def handle(request):
+    async def handle(self, request):
         # Button has been pressed
         mute = self.bot.get_channel(870946768928534528)
         mute.send('The Button has been pressed')
         return web.Response(text='success')
 
+    async def start_app(self):
+        web.run_app(self.app, port=8080)
+
 
 
 async def setup(bot):
-    web.run_app(app, port=8080)
     await bot.add_cog(HTTPCog(bot))
+    httpcog = bot.get_cog('HTTPCog')
+    if httpcog is not None:
+        httpcog.start_app()
+    
+    
