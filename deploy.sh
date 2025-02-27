56c39e7f667c1b1d6c77c0927310cbbf5b926a36
Nick Eickhoff <eickh032@umn.edu>
2025-02-27 00:44:48 -0600

    fix deploy dirs

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-cd /home/pi/discord-bot
+cd /home/pi/app
 
 # Activate Python environment and install dependencies
-source venv/bin/activate
+source /home/pi/app/venv/bin/activate
 pip install -r requirements.txt
 
 # Restart the bot
