3f2ab11a00f41712d64be58e44837265e5802f11
Nick Eickhoff <nickeick@comcast.net>
2025-04-01 02:13:49 -0500

    Added price to server mute command

--- src/cogs/voice_cog.py
+++ src/cogs/voice_cog.py
@@ -20,11 +20,19 @@ class VoiceCog(commands.Cog):
     
     @checks.checks.is_in_dojo()
     @checks.checks.has_voice_state()
-    @app_commands.command(description='Server mute someone in call for 1 minute at the cost of 1 brain cell')
+    @app_commands.command(description='COSTS 1 COMMON CENT: Server mute someone in call for 1 minute')
     async def mute(self, interaction: Interaction, member: Member):
         if member.voice is not None:
+            if member.voice.channel.guild.id != self.dojo_id:
+                await interaction.response.send_message(f"You can only mute someone in a Dojo voice channel")
+                return
+            points = await self.bot.db_manager.get_brain_cells(interaction.user.name)
+            if points < 1:
+                await interaction.response.send_message(f"You don't have enough common cents (1) to use this command")
+                return
+            await self.bot.db_manager.remove_brain_cells(interaction.user.name, 1)
             await member.edit(mute=True)
-            await interaction.response.send_message(f"You have muted {member.nick} for 60 seconds")
+            await interaction.response.send_message(f"You have spent 1 Common Cent to mute {member.nick} for 60 seconds")
             await asyncio.sleep(60)
             await member.edit(mute=False)
         else:
